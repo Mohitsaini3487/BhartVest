2c48ecb4730f4b49566389cec0c25f80235c225f
Mohit Saini <mohitdsaini.2005@gmail.com>
2025-11-06 20:08:50 +0530

    Updated Firebase config with environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-8938119796-da63d",
-  "appId": "1:359371345203:web:831e29f9fb531276838246",
-  "apiKey": "AIzaSyAlk1Covl7EpzVezgsnLG3gw76Q2jRLuOY",
-  "authDomain": "studio-8938119796-da63d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "359371345203"
+  apiKey: "AIzaSyAlk1Covl7EpzVezgsnLG3gw76Q2jRLuOY",
+  authDomain: "studio-8938119796-da63d.firebaseapp.com",
+  projectId: "studio-8938119796-da63d",
+  storageBucket: "studio-8938119796-da63d.firebasestorage.app",
+  messagingSenderId: "359371345203",
+  appId: "1:359371345203:web:831e29f9fb531276838246"
 };
